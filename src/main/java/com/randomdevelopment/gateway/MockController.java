7c06ed55fcbb5992a2fb59cc7853683780a3fc8f
hibron <mpoznanski@interia.pl>
2019-04-09 14:15:40 +0200

    init commit with mock working

---
+++ src/main/java/com/randomdevelopment/gateway/MockController.java
@@ -0,0 +1,347 @@
+package com.randomdevelopment.gateway;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@ComponentScan
+@RequestMapping("mock")
+public class MockController {
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors")
+	public String monitors() {
+		return "[\r\n" + 
+				"    {\r\n" + 
+				"        \"name\": \"monitor_hdmi\",\r\n" + 
+				"        \"resources\": \r\n" + 
+				"        [\r\n" + 
+				"            {\r\n" + 
+				"                    \"name\": \"komp_pod_biurkiem\",\r\n" + 
+				"                    \"metrics\": [\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"cpu\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }\r\n" + 
+				"                    ]\r\n" + 
+				"            },\r\n" + 
+				"            {\r\n" + 
+				"                    \"name\": \"komp_za_sciana\",\r\n" + 
+				"                    \"metrics\": [\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"temp\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }\r\n" + 
+				"                    ]\r\n" + 
+				"            }\r\n" + 
+				"        ]\r\n" + 
+				"    },\r\n" + 
+				"    {\r\n" + 
+				"        \"name\": \"monitor_komplex\",\r\n" + 
+				"        \"resources\": \r\n" + 
+				"        [\r\n" + 
+				"            {\r\n" + 
+				"                    \"name\": \"przegrzewajacy_sie_komp\",\r\n" + 
+				"                    \"metrics\": [\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"temp\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        },  \r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"cpu\",\r\n" + 
+				"                            \"type\": \"complex\",\r\n" + 
+				"                            \"userId\": \"132\",\r\n" + 
+				"                            \"sourceMetric\": \"cpu\",\r\n" + 
+				"                            \"period\": 180,\r\n" + 
+				"                            \"interval\": 60\r\n" + 
+				"                        }\r\n" + 
+				"                    ]\r\n" + 
+				"            }\r\n" + 
+				"        ]\r\n" + 
+				"    },\r\n" + 
+				"    {\r\n" + 
+				"        \"name\": \"monitor_dsub\",\r\n" + 
+				"        \"resources\": \r\n" + 
+				"        [\r\n" + 
+				"            {\r\n" + 
+				"                    \"name\": \"komputer_w_stodole\",\r\n" + 
+				"                    \"metrics\": [\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"cpu\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }\r\n" + 
+				"                    ]\r\n" + 
+				"            }\r\n" + 
+				"        ]\r\n" + 
+				"    },\r\n" + 
+				"    {\r\n" + 
+				"        \"name\": \"monitor_bez_kompa\",\r\n" + 
+				"        \"resources\": \r\n" + 
+				"        [\r\n" + 
+				"        ]\r\n" + 
+				"    }\r\n" + 
+				"]";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_hdmi")
+	public String monitorsHdmi() {
+		return " [\r\n" + 
+				"            {\r\n" + 
+				"                    \"name\": \"komp_pod_biurkiem\",\r\n" + 
+				"                    \"metrics\": [\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"cpu\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }\r\n" + 
+				"                    ]\r\n" + 
+				"            },\r\n" + 
+				"            {\r\n" + 
+				"                    \"name\": \"komp_za_sciana\",\r\n" + 
+				"                    \"metrics\": [\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"temp\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }\r\n" + 
+				"                    ]\r\n" + 
+				"            }\r\n" + 
+				"]";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_komplex")
+	public String monitorsKomplex() {
+		return "[\r\n" + 
+				"            {\r\n" + 
+				"                    \"name\": \"przegrzewajacy_sie_komp\",\r\n" + 
+				"                    \"metrics\": [\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"temp\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        },  \r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"cpuk\",\r\n" + 
+				"                            \"type\": \"complex\",\r\n" + 
+				"                            \"userId\": \"132\",\r\n" + 
+				"                            \"sourceMetric\": \"cpu\",\r\n" + 
+				"                            \"period\": 180,\r\n" + 
+				"                            \"interval\": 60\r\n" + 
+				"                        }\r\n" + 
+				"                    ]\r\n" + 
+				"            }\r\n" + 
+				"        ]";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_dsub")
+	public String monitorsDsub() {
+		return "[\r\n" + 
+				"            {\r\n" + 
+				"                    \"name\": \"komputer_w_stodole\",\r\n" + 
+				"                    \"metrics\": [\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"cpu\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }\r\n" + 
+				"                    ]\r\n" + 
+				"            }\r\n" + 
+				"]\r\n" + 
+				"";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_bez_kompa")
+	public String monitorsBezK() {
+		return "[]";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_hdmi/resources/komp_pod_biurkiem")
+	public String monitorsResKompP() {
+		return "{\r\n" + 
+				"                    \"name\": \"komp_pod_biurkiem\",\r\n" + 
+				"                    \"metrics\": [\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"cpu\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }\r\n" + 
+				"                    ]\r\n" + 
+				"            }";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_hdmi/resources/komp_za_sciana")
+	public String monitorsResKompZa() {
+		return "{\r\n" + 
+				"                    \"name\": \"komp_za_sciana\",\r\n" + 
+				"                    \"metrics\": [\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"temp\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }\r\n" + 
+				"                    ]\r\n" + 
+				"            }";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_komplex/resources/przegrzewajacy_sie_komp")
+	public String monitorsResPrzeg() {
+		return "{\r\n" + 
+				"                    \"name\": \"przegrzewajacy_sie_komp\",\r\n" + 
+				"                    \"metrics\": [\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"temp\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        },  \r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"cpuk\",\r\n" + 
+				"                            \"type\": \"complex\",\r\n" + 
+				"                            \"userId\": \"132\",\r\n" + 
+				"                            \"sourceMetric\": \"cpu\",\r\n" + 
+				"                            \"period\": 180,\r\n" + 
+				"                            \"interval\": 60\r\n" + 
+				"                        }\r\n" + 
+				"                    ]\r\n" + 
+				"            }";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_dsub/resources/komputer_w_stodole")
+	public String monitorsResKompWS() {
+		return " {\r\n" + 
+				"                    \"name\": \"komputer_w_stodole\",\r\n" + 
+				"                    \"metrics\": [\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"cpu\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }\r\n" + 
+				"                    ]\r\n" + 
+				"            }";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_hdmi/resources/komp_pod_biurkiem/metrics")
+	public String monitors1() {
+		return "[\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"cpu\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }\r\n" + 
+				"                    ]";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_hdmi/resources/komp_za_sciana/metrics")
+	public String monitors2() {
+		return "[\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"temp\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }\r\n" + 
+				"                    ]";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_komplex/resources/przegrzewajacy_sie_komp/metrics")
+	public String monitors3() {
+		return "[\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"temp\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        },  \r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"cpuk\",\r\n" + 
+				"                            \"type\": \"complex\",\r\n" + 
+				"                            \"userId\": \"132\",\r\n" + 
+				"                            \"sourceMetric\": \"cpu\",\r\n" + 
+				"                            \"period\": 180,\r\n" + 
+				"                            \"interval\": 60\r\n" + 
+				"                        }\r\n" + 
+				"                    ]";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_dsub/resources/komputer_w_stodole/metrics")
+	public String monitors4() {
+		return "[\r\n" + 
+				"                        {\r\n" + 
+				"                            \"name\": \"cpu\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }\r\n" + 
+				"                    ]";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_hdmi/resources/komp_pod_biurkiem/metrics/cpu")
+	public String monitors5() {
+		return "{\r\n" + 
+				"                            \"name\": \"cpu\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_hdmi/resources/komp_za_sciana/metrics/temp")
+	public String monitors6() {
+		return "{\r\n" + 
+				"                            \"name\": \"temp\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_komplex/resources/przegrzewajacy_sie_komp/metrics/temp")
+	public String monitors7() {
+		return "{\r\n" + 
+				"                            \"name\": \"temp\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_komplex/resources/przegrzewajacy_sie_komp/metrics/cpuk")
+	public String monitors8() {
+		return "{\r\n" + 
+				"                            \"name\": \"cpuk\",\r\n" + 
+				"                            \"type\": \"complex\",\r\n" + 
+				"                            \"userId\": \"132\",\r\n" + 
+				"                            \"sourceMetric\": \"cpu\",\r\n" + 
+				"                            \"period\": 180,\r\n" + 
+				"                            \"interval\": 60\r\n" + 
+				"                        }";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/monitor_dsub/resources/komputer_w_stodole/metrics/cpu")
+	public String monitors9() {
+		return "{\r\n" + 
+				"                            \"name\": \"cpu\",\r\n" + 
+				"                            \"type\": \"normal\"\r\n" + 
+				"                        }";
+	}
+	
+	@GetMapping
+	@RequestMapping(produces = "application/json", value = "/monitors/{mname}/resources/{rname}/metrics/{mnamee}/measurements")
+	public String monitors10() {
+		return "{\r\n" + 
+				"    \"columns\": [\"order\", \"timestamp\", \"value\"],\r\n" + 
+				"    \"data\": [\r\n" + 
+				"        [\"1\", \"1554663615\", \"213\"],\r\n" + 
+				"        [\"2\", \"1554663616\", \"214\"],\r\n" + 
+				"        [\"3\", \"1554663617\", \"215\"],\r\n" + 
+				"        [\"4\", \"1554663618\", \"264\"],\r\n" + 
+				"        [\"5\", \"1554663619\", \"214\"]\r\n" + 
+				"    ]\r\n" + 
+				"}";
+	}
+	
+	@DeleteMapping
+	@PostMapping
+	@RequestMapping(value = "/monitors/{mname}/resources/{kname}/metrics")
+	public String monitors11() {
+		return "dziekujemy za dodanie/usuniecie metryki kompleksowej ";
+	}
+}
